fb71b5796faa8cad83dce94cca65f8f47d8926c2
curtischatch <curtis.chat.hk@gmail.com>
2018-07-30 19:53:29 -0400

    1&2 done

---
+++ exercise.rb
@@ -0,0 +1,26 @@
+{ data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+# 1 want capacity of room 201 and store in variable
+
+capacity = hash [:data][:rooms][0][capacity]
+p capacity
+
+# 2
+
+hash[:data][:events].each do |array|
+
+  if array[:room] == 1
+    array[:attendees] < 50
+    p "OK"
+  end
+end
